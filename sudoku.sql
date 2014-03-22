36b57c1e8a2d3e7b89701258b77e08d15bfe7ddb
Grief-Code <hasgrief@googlemail.com>
2014-03-22 09:27:51 +0000

    Fixed database settngs and added dummy entry for the datatbase
    nickname: ms
    password: test

--- sudoku.sql
+++ sudoku.sql
@@ -1,4 +1,3 @@
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
@@ -25,9 +24,16 @@ CREATE TABLE IF NOT EXISTS `accounts` (
   `first_name` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
   `last_name` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
   `username` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `password` varchar(28) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+--
+-- Daten für Tabelle `accounts`
+--
+
+INSERT INTO `accounts` (`id`, `first_name`, `last_name`, `username`, `password`) VALUES
+(1, 'Michael', 'Schneider', 'ms', '098f6bcd4621d373cade4e832627b4f6');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
